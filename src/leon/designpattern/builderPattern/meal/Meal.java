a6a0430471af70073135b99825d1503dad9ef264
wanghuan <1958862281@qq.com>
2019-04-26 19:19:48 +0800

    "4-29 commit all some pritices"

--- src/leon/designpattern/builderPattern/meal/Meal.java
+++ src/leon/designpattern/builderPattern/meal/Meal.java
@@ -13,6 +13,7 @@ public class Meal {
     }
 
     public float getCost(){
+
         float cost = 0.0f;
         for (Item item : items) {
             cost += item.price();
@@ -21,6 +22,7 @@ public class Meal {
     }
 
     public void showItems(){
+
         for (Item item : items) {
             System.out.print("Item : "+item.name());
             System.out.print(", Packing : "+item.packing().pack());
